61bca623b5926645f42f8c762003fe2824843212
EC2 Default User <ec2-user@ip-172-31-11-35.ap-northeast-1.compute.internal>
2022-07-11 04:38:34 +0000

    [Finish 8b] Rails6-bookers2-debug-main.b

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-//import Turbolinks from "turbolinks"
+import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
@@ -13,8 +13,8 @@ import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application"
 
-import "chartkick"
-import "chart.js"
+import Chart from 'chart.js/auto';
 Rails.start()
-//Turbolinks.start()
+Turbolinks.start()
 ActiveStorage.start()
+global.Chart = Chart;
